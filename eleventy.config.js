45002fe8fbe09d68bfccd321018aa3c69fb8f982
udnzk <251772542+udnzk@users.noreply.github.com>
2026-09-08 22:15:29 +0300

    changed the source location

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,13 @@
 module.exports = function(eleventyConfig) {
+	eleventyConfig.addPassthroughCopy("src/CNAME");
+	eleventyConfig.addPassthroughCopy("src/favicon.ico");
+	eleventyConfig.addPassthroughCopy("src/fonts");
+	eleventyConfig.addPassthroughCopy("src/css");
 	return {
 	   markdownTemplateEngine: "njk",
-	   htmlTemplateEngine: "njk"
+	   htmlTemplateEngine: "njk",
+	   dir: {
+		input: "src"
+	   },
 	};
 };

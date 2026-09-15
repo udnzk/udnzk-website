c6ccaeb77f3651ca1164ffb1c64eacad95b71abb
udnzk <251772542+udnzk@users.noreply.github.com>
2026-09-15 18:12:32 +0300

    added post structure and a posted something very basic

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,6 +3,7 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("src/favicon.ico");
 	eleventyConfig.addPassthroughCopy("src/fonts");
 	eleventyConfig.addPassthroughCopy("src/css");
+	eleventyConfig.addPassthroughCopy("src/media")
 	return {
 	   markdownTemplateEngine: "njk",
 	   htmlTemplateEngine: "njk",
---
+++ src/posts/2026/09/15/orange-cat.md
@@ -0,0 +1,6 @@
+---
+title: Cat.
+date: 2026-09-15
+---
+![An orange cat sleeping on a laptop keyboard](/media/images/cat1.jpg)
+Isn't she cute?
